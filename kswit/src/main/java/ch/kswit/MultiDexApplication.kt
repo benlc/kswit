22ef2ff09f97780d966d6addeeafd80ed4d18193
benlc <benjamin.lecun@gmail.com>
2020-01-20 11:49:58 -0600

    Open class

--- kswit/src/main/java/ch/kswit/MultiDexApplication.kt
+++ kswit/src/main/java/ch/kswit/MultiDexApplication.kt
@@ -2,7 +2,7 @@ package ch.kswit
 
 import androidx.multidex.MultiDexApplication
 
-class MultiDexApplication: MultiDexApplication() {
+open class MultiDexApplication: MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
